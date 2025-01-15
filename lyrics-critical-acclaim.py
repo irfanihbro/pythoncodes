10fcf4c3c1d11556ed727a880d8b4ffd16a26cd5
Irfan Anshari <irfananshr.id@gmail.com>
2025-01-15 18:18:31 +0800

    Add files via upload

---
+++ lyrics-critical-acclaim.py
@@ -0,0 +1,39 @@
+import time
+from threading import Thread, Lock
+import sys
+
+lock = Lock()
+
+def animate_text(text, delay=0.1):
+    with lock:
+        for char in text:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(delay)
+        print()
+
+def sing_lyric(lyric, delay, speed):
+    time.sleep(delay)
+    animate_text(lyric, speed)
+
+def sing_song():
+    lyrics = [
+        ("Excuse the obscene, ignore the untrue", 0.115),
+        ("Depictions we see try and get through", 0.135),
+        ("Admitting mistakes can hurt", 0.14),
+        ("I'm not the last but I sure ain't the first", 0.115),                                
+    ]
+    delays = [0.0, 4.5, 10.5, 15.0]
+    
+    threads = []
+    for i in range(len(lyrics)):
+        lyric, speed = lyrics[i]
+        t = Thread(target=sing_lyric, args=(lyric, delays[i], speed))
+        threads.append(t)
+        t.start()
+    
+    for thread in threads:
+        thread.join()
+
+if __name__ == "__main__":
+    sing_song()
